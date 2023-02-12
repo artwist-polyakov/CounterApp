36408f261931d14fededd2eacec63d3dfac76569
Aleksandr Poliakov <master@artwist.ru>
2023-02-12 12:36:54 +0300

    Практическая работа 1: минимальные задачи

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter: Int = 0
 
+    @IBOutlet weak var counterValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        print("Запущено, значение сounter = \(counter)")
+        counterValue.text = "\(counter)"
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTap(_ sender: Any) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.prepare()
+        generator.impactOccurred()
+        counter+=1
+        counterValue.text = "\(counter)"
+//        print("Кнопка нажата")
+//        print("Значение counter = \(counter)")
+    }
+    
 }
 
