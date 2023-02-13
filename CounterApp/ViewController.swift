5cba60b3d45ee6dbd6dee493465d7be35d2d7ae2
Aleksandr Poliakov <master@artwist.ru>
2023-02-13 13:36:46 +0300

    Отцентровано хорошо. TODO: При старте приложения счетчик = 0. Но было бы хорошо, чтобы он сохранялся из прошлых сессий. Добавить жест смахивания влево, чтобы счетчик уменьшался на 1. Добавить онбординг экран.

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController {
     var counter: Int = 0
 
     @IBOutlet weak var counterValue: UILabel!
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        print("Запущено, значение сounter = \(counter)")
         counterValue.text = "\(counter)"
+        
         // Do any additional setup after loading the view.
     }
 
@@ -29,6 +32,6 @@ class ViewController: UIViewController {
 //        print("Кнопка нажата")
 //        print("Значение counter = \(counter)")
     }
-    
+
 }
 
