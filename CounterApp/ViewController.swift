b920babbee544bfaa572b1b7ff8d164d8712f994
Aleksandr Polyakov <master@artwist.ru>
2023-02-14 22:49:24 +0300

    Заработало

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -12,14 +12,9 @@ class ViewController: UIViewController {
     
     private var counter: Int = 0
     @IBOutlet weak private var counterValue: UILabel!
-
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print("Запущено, значение сounter = \(counter)")
-        
-        
         //если в памяти устройства есть значение для счетчика
         if UserDefaults.standard.value(forKey:"counter" ) != nil {
             //присваиваем нашему счетчику
@@ -27,54 +22,25 @@ class ViewController: UIViewController {
         }
         counterValue.text = "\(counter)"
         
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: Selector(("respondToSwipeGesture:")))
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(onSwipeRight))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-            self.view.addGestureRecognizer(swipeRight)
+        self.view.addGestureRecognizer(swipeRight)
 
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: Selector(("respondToSwipeGesture:")))
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(onSwipeLeft))
         swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
         self.view.addGestureRecognizer(swipeLeft)
-
-        
     }
 
     @IBAction private func buttonTap(_ sender: Any) {
         let generator = UIImpactFeedbackGenerator(style: .medium)
         generator.prepare()
         generator.impactOccurred()
-        counter+=1
-        counterValue.text = "\(counter)"
-        //сохраняем переменную счетчика в UserDefaults
-        UserDefaults.standard.setValue(counter, forKey: "counter")
-//        print("Кнопка нажата")
-//        print("Значение counter = \(counter)")
+        counterIncrimention ()
     }
     
-    private func respondToSwipeGesture(gesture: UIGestureRecognizer)
-    {
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer
-        {
-            switch swipeGesture.direction
-            {
-                case UISwipeGestureRecognizer.Direction.right:
-                         //write your logic for right swipe
-                    counerIncrimention()
-                        
-
-                case UISwipeGestureRecognizer.Direction.left:
-                         //write your logic for left swipe
-                    counterDecrimention ()
-
-                default:
-                    break
-            }
-        }
-    }
-    
-    private func counerIncrimention () {
+    private func counterIncrimention () {
         counter+=1
         counterValue.text = "\(counter)"
-        //сохраняем переменную счетчика в UserDefaults
         UserDefaults.standard.setValue(counter, forKey: "counter")
     }
     
@@ -87,6 +53,17 @@ class ViewController: UIViewController {
         
     }
     
-    
+}
+
+private extension ViewController {
+  @objc
+  func onSwipeRight() {
+      counterIncrimention ()
+  }
+
+  @objc
+  func onSwipeLeft() {
+      counterDecrimention ()
+  }
 }
 
